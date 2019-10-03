a37bdacacb69769594840f94bb1bc4c8dfdf2005
peter kazarinoff <peter.kazarinoff@pcc.edu>
2019-10-03 11:01:17 -0700

    started objects module

---
+++ objects.py
@@ -0,0 +1,88 @@
+# objects.py
+
+"""
+module for the objects in the schedule app
+
+
+"""
+
+
+class TimeBlock:
+    def __init__(
+        self,
+        id=None,
+        start_time=None,
+        end_time=None,
+        day=None,
+        CRN=None,
+        room=None,
+        course_code=None,
+        course_name=None,
+        instructor=None,
+        is_class=True,
+        is_office_hours=False,
+        quarter=None,
+        year=None,
+    ):
+        self.id = id  # int
+        self.start_time = start_time  # datetime
+        self.end_time = end_time  # datetime
+        self.day = day  # str
+        self.CNR = CRN  # int
+        self.room = room  # str
+        self.course_code = course_code  # str
+        self.course_name = course_name  # str
+        self.instructor = instructor  # str
+        self.is_class = is_class # bool
+        self.is_office_hours = is_office_hours # bool
+        self.quarter = quarter # str
+        self.year = year # int
+
+    def __str__(self):
+        if self.day and self.start_time and self.end_time and self.course_code and self.room:
+            return self.day + self.start_time + self.end_time + self.course_code + self.room
+        else:
+            return ''
+
+
+class Instructor():
+    def __init__(self, first_name=None,
+    last_name=None,
+    office=None,
+    email=None,
+    phone=None,
+    office_hours=None,):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.office = office
+        self.email = email
+        self.phone = phone
+        self.office_hours = office_hours  # list of TimeBlocks
+
+
+class OfficeHour(TimeBlock):
+    def __init__(self, is_class=False, is_office_hours=True):
+        super().__init__(self)
+
+
+class Room():
+    def __init__(self, location):
+        self.location = location
+
+class Day():
+    def __init__(self,in_arg):
+        day_dict = {'Mon': 'Monday', 'Tues': 'Tuesday', 'Wed': 'Wednesday', 'Thurs': 'Thursday', 'Fri': 'Friday',
+                    'Sat': 'Saturday', 'Sun': 'Sunday'}
+        num_dict = {1: 'Mon', 2: 'Tues', 3: 'Wed', 4: 'Thurs', 5: 'Fri',
+                    6: 'Sat', 7: 'Sun'}
+        if type(in_arg) == int and in_arg in [1,2,3,4,5,6,7]:
+            self.number = in_arg
+            self.short_name = num_dict[in_arg]
+            self.long_name = day_dict[self.short_name]
+        elif in_arg in day_dict.keys():
+            self.short_name = in_arg
+            self.long_name = day_dict[in_arg]
+        elif in_arg in day_dict.values():
+            self.long_name = in_arg
+        else:
+            return TypeError('Only 1-7, "Mon" or "Monday" can be used to create a Day object')
